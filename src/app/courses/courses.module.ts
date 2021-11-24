b937349b2525cab6de0a140264d2be71ddecc0b0
Jose Junior <jose.junior@porlande.com>
2021-11-24 23:38:56 +0100

    sharing material modules

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
@@ -10,9 +10,10 @@ import { CoursesComponent } from './courses/courses.component';
     CoursesComponent
   ],
   imports: [
-    CommonModule,
+
+  CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
