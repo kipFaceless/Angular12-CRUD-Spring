76eeb948989957c9bebb6573d36e7bb250e8e466
Jose Junior <jose.junior@porlande.com>
2021-12-19 00:59:36 +0100

    calling spring API

---
+++ spring-crud/crud-angular/src/main/java/com/eusebio/crudangular/CrudAngularApplication.java
@@ -0,0 +1,33 @@
+package com.eusebio.crudangular;
+
+import com.eusebio.crudangular.model.Course;
+import com.eusebio.crudangular.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAngularApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase( CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+				
+			};
+
+		}
+	}
+
+
